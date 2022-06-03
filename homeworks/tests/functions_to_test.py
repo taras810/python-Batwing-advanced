88d8a11d1b0ae54d56b8f7a93704cba30b45d104
Luzhnyi, Liubomyr <liubomyr.luzhnyi@edvantis.com>
2022-06-03 21:18:13 +0300

    databases

--- homeworks/tests/functions_to_test.py
+++ homeworks/tests/functions_to_test.py
@@ -1,18 +1,21 @@
 class Calculator:
     """ A simple calculator App"""
-
+    @staticmethod
     def add(x, y):
         """Add Function"""
         return x + y
 
+    @staticmethod
     def subtract(x, y):
         """Subtract Function"""
         return x - y
 
+    @staticmethod
     def multiply(x, y):
         """Multiply Function"""
         return x * y
 
+    @staticmethod
     def divide(x, y):
         """Divide Function"""
         if y == 0:
