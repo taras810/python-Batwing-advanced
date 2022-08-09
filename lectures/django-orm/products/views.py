a0773092c5afc7a6f43a436902720ddc188c30a5
Luzhnyi, Liubomyr <liubomyr.luzhnyi@edvantis.com>
2022-08-09 21:05:51 +0300

    Django ORM

---
+++ lectures/django-orm/products/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product
+
+
+def add_product(request):
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            return render(request, "products/add.html")
+        else:
+            product = Product()
+            product.title = request.POST.get("title")
+            product.description = request.POST.get("description")
+            product.user = request.user
+            product.save()
+            return redirect("/")
+    else:
+        return redirect("/")
+
+
+def product_details(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, "products/details.html", {"product": product})
